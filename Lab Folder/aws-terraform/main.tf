32b76f788d0cc2960bd4490f445e0f5152359a39
Los-merengue <michaels4uandme@gmail.com>
2023-02-07 00:26:07 +0300

    Added vpc group and ami

--- Lab Folder/aws-terraform/main.tf
+++ Lab Folder/aws-terraform/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   profile = "default"
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "ls-lab3-terraform-1" {
+resource "aws_instance" "ls-terraform" {
   ami = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
   key_name = "LS3"
-  vpc_security_group_ids = ["sg-05a6e7236cefff274"]
+  vpc_security_group_ids = ["sg-05d89bbcc1dfb2f5b"]
 
   tags = {
     Name = "Ubuntu"
