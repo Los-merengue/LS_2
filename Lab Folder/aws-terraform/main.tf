deb2e453e310a8958bd406c5660798176454ee71
Los-merengue <michaels4uandme@gmail.com>
2023-02-07 00:41:50 +0300

    Modify the security group from all to few traffic

--- Lab Folder/aws-terraform/main.tf
+++ Lab Folder/aws-terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ls-terraform" {
   ami = "ami-00874d747dde814fa"
   instance_type = "t2.micro"
   key_name = "LS2"
-  vpc_security_group_ids = ["sg-05d89bbcc1dfb2f5b"]
+  vpc_security_group_ids = ["sg-0f251bc5451edffda"]
 
   tags = {
     Name = "Ubuntu"
